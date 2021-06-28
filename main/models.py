b566ae7a54c65a9a325710bc5fce991eba935485
nick-casa <nc16@princeton.edu>
2021-06-28 13:02:02 -0400

    login

--- main/models.py
+++ main/models.py
@@ -2,3 +2,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.db import models
 
 # Create your models here.
+
+class UserDetails(models.Model):
+    user_id = models.IntegerField()
+    email = models.EmailField()
+    wallet = models.CharField(max_length=100)
+
